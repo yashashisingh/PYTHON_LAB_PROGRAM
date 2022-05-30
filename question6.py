2b3b78e63be5e038a6db696ee7359b965591ed71
yashashvi singh <106493713+yashashisingh@users.noreply.github.com>
2022-05-30 09:31:47 +0530

    Create question6.py

---
+++ question6.py
@@ -0,0 +1,11 @@
+dict1={}
+size=int(input("enter size"))
+for i in range (0,size):
+    key=int(input())
+    value=int(input())
+    dict1.update({key:vaue})
+print(max(dict1.values()),'',end="")
+print(min(dict1.values()),' ',end="")
+    
+    
+    
